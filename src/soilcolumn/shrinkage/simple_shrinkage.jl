79ad5e91d0261beac1bf5a3132aa0586fa974cea
Bas Knaake <bas.knaake@gmail.com>
2023-03-22 15:46:34 +0100

    New calculation of shrinkage and added tau and r to SimpleShrinkage

--- src/soilcolumn/shrinkage/simple_shrinkage.jl
+++ src/soilcolumn/shrinkage/simple_shrinkage.jl
@@ -2,16 +2,22 @@
 struct SimpleShrinkage <: ShrinkageProcess
     Δz::Float  # cell thickness
     n::Float # shrinkage degree
+    τ::Float # time constant for shrinkage related to soiltype
+    r::Float # direction of shrinkage, 3 = isotrope
     shrinkage::Float # computed shrinkage
 end
 
-function shrink(ss::SimpleShrinkage, Δt::Float64, r::Int64)
-    n_residual = 0.7
-    Δn = n_residual - ss.n
-    n = ss.n + Δn * (exp(-Δt) - 1)
 
-    shrinkage = 1.0
-    Δz =
-    # TODO: volume and γ update?
-        return SimpleShrinkage(Δz, n, shrinkage)
+function shrink(ss::SimpleShrinkage, Δt::Float64)
+    n_residual = 0.7
+    n_next = ss.n + (ss.n - n_residual) * (exp(-Δt / ss.τ) - 1)
+
+    Δn = n_next - ss.n
+
+    factor = 0.001 #TODO: how to determine factor? From subsurface model?
+    shr = (factor * Δn)^(1 / ss.r)
+
+    shrinkage = min(ss.Δz, shr)
+
+    return SimpleShrinkage(Δz, n_next, ss.τ, ss.r, shrinkage)
 end
