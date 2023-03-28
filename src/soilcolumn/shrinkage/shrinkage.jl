b740e8b95b6443472e0abeb314ef697e982ce97d
Bas Knaake <bas.knaake@gmail.com>
2023-03-28 10:32:14 +0200

    Correct initialization of shrinkage column

--- src/soilcolumn/shrinkage/shrinkage.jl
+++ src/soilcolumn/shrinkage/shrinkage.jl
@@ -1,3 +1,15 @@
+"""
+    ShrinkageColumn{S}(cells, z, Δz, result, max_shrinkage_depth)
+
+Collection of SimpleShrinkage cells to compute shrinkage for. 
+
+# Arguments:
+- `cells::Vector{S}`: Collection of SimpleShrinkage structs.
+- `z::Vector{Float}`: Depth of the cells.
+- `Δz::Vector{Float}`: Thickness of the cells.
+- `result::Vector{Float}`: Computed shrinkage of each cell. All NaNs at t=0.
+- `max_shrinkage_depth::Float`: Maximum depth to compute shrinkage for in cells.
+"""
 struct ShrinkageColumn{S}
     cells::Vector{S}
     z::Vector{Float}  # cell center
@@ -6,16 +18,21 @@ struct ShrinkageColumn{S}
     max_shrinkage_depth::Float
 end
 
+
 shrinkage_depth(column::ShrinkageColumn{NullShrinkage}, _, _, _, _) = nothing
 shrink!(column::ShrinkageColumn{NullShrinkage}, _) = nothing
 synchronize_z!(column::ShrinkageColumn{NullShrinkage}, __) = nothing
+
 
 function shrink!(
     column::ShrinkageColumn{S},
     phreatic_level::Float,
     Δt::Float,
 ) where {S<:ShrinkageProcess}
-    shrinkage_z = max(phreatic_level, surface_level(column) - column.max_shrinkage_depth)
+    shrinkage_z = max(
+        phreatic_level,
+        surface_level(column) - column.max_shrinkage_depth
+    )
     column.result .= 0.0
     for index in reverse(1:length(column.cells))
         if column.z[index] > shrinkage_z
@@ -31,7 +48,7 @@ function shrink!(
 end
 
 
-function synchronize!(column::ShrinkageColumn{S}, Δz) where {S<:ShrinkageProcess}
+function synchronize_z!(column::ShrinkageColumn{S}, Δz) where {S<:ShrinkageProcess}
     for i = 1:length(column.cells)
         cell = column.cells[i]
         newcell = @set cell.Δz = Δz[i]
@@ -42,12 +59,15 @@ end
 
 
 function initialize(::Type{SimpleShrinkage}, domain, subsoil, I)
-    n = fetch_field(shrinkage_degree, :, I, domain)
-    n_residual = fetch_field(residual_shrinkage_degree, :, I, domain)
+    n = fetch_field(shrinkage_degree, :, I, domain) #TODO: what is fieldname for shrinkage factor in parameter table?
+    max_shrinkage_depth = subsoil.data[:phreatic_level][I] + 0.3 #TODO: is prheatic level in nc correct to use for GLG?
+    τ = 60.0 # Time dependent factor for shrinkage process
+    r = 3.0 # Direction of shrinkage is assumed isoptropic (r=3)
 
     cells = Vector{SimpleShrinkage}()
+
     for (i, Δz) in enumerate(domain.Δz)
-        cell = SimpleShrinkage(Δz, n[i], n_residual[i], NaN)
+        cell = SimpleShrinkage(Δz, n[i], τ, r, NaN)
         push!(cells, cell)
     end
 
@@ -56,7 +76,7 @@ function initialize(::Type{SimpleShrinkage}, domain, subsoil, I)
         domain.z,
         domain.Δz,
         fill(NaN, domain.n),
-        2 # TODO: find out how to determine max shrinkage depth 
+        max_shrinkage_depth
     )
     return column
 
