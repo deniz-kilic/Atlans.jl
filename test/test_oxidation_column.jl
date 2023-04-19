f88f7d7a6c9919128c116e37b787618979d66246
Bas Knaake <bas.knaake@gmail.com>
2023-04-19 15:47:32 +0200

    Added simple OxidationColumn tests

--- test/test_oxidation_column.jl
+++ test/test_oxidation_column.jl
@@ -1,5 +1,37 @@
 @testset "OxidationColumn" begin
+    column = AtlansFixtures.carbon_store_column()
+
+    @testset "constructor" begin
+        @test typeof(column) == Atlans.OxidationColumn{
+            Atlans.CarbonStore
+        }
+    end
+
     @testset "surface_level" begin
-        #@test Atlans.surface_level(column) ≈ 4.0
+        @test Atlans.surface_level(column) ≈ 4.0
+    end
+
+    @testset "oxidation_depth" begin
+        phreatic = 2.5
+        oz = max(
+            phreatic,
+            Atlans.surface_level(column) - column.max_oxidation_depth
+        )
+        @test oz ≈ 3.0
+    end
+
+    @testset "oxidate" begin
+        phreatic = 2.5
+        Δt = 3650.0
+
+        Atlans.oxidate!(column, phreatic, Δt)
+
+        expected_ox = [0.0, 0.0, 0.0, 9.125e-3]
+        expected_forg = [0.2, 0.2, 0.2, 0.19706930295578862]
+
+        @test all(column.result .≈ expected_ox)
+        @test all(
+            c.f_organic ≈ forg for (c, forg) in zip(column.cells, expected_forg)
+        )
     end
 end
