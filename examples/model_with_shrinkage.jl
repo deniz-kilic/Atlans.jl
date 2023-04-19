9b327ec84baa5d4926e74584a4cd4c08535e4f3b
Bas Knaake <bas.knaake@gmail.com>
2023-04-19 15:45:28 +0200

    Changed to conceptual model

--- examples/model_with_shrinkage.jl
+++ examples/model_with_shrinkage.jl
@@ -1,9 +1,91 @@
 using Atlans
+using CSV
+using NCDatasets
+using DataFrames
+
+function create_xcoord!(ds, x)
+    defVar(
+        ds,
+        "x",
+        x,
+        ("x",),
+        attrib=["standard_name" => "projection_x_coordinate", "axis" => "X"],
+    )
+end
 
 
-workdir = raw"c:\Users\knaake\OneDrive - Stichting Deltares\Documents\atlans_test\implementatie_rijping"
-path_nc = joinpath(workdir, "subsoil-model-fase2.nc")
-path_csv = joinpath(workdir, "parameters.csv")
+function create_ycoord!(ds, y)
+    defVar(
+        ds,
+        "y",
+        y,
+        ("y",),
+        attrib=["standard_name" => "projection_y_coordinate", "axis" => "Y"],
+    )
+end
+
+
+function subsoil_netcdf()
+    filename = tempname()
+    ds = NCDatasets.Dataset(filename, "c")
+    defDim(ds, "x", 2)
+    defDim(ds, "y", 3)
+    defDim(ds, "layer", 4)
+    create_xcoord!(ds, [12.5, 37.5])
+    create_ycoord!(ds, [87.5, 62.5, 37.5])
+
+    geology = defVar(ds, "geology", Int, ("layer", "x", "y"))
+    lithology = defVar(ds, "lithology", Int, ("layer", "x", "y"))
+    thickness = defVar(ds, "thickness", Float64, ("layer", "x", "y"))
+    phreatic = defVar(ds, "phreatic_level", Float64, ("x", "y"))
+    base = defVar(ds, "zbase", Float64, ("x", "y"))
+    domainbase = defVar(ds, "domainbase", Float64, ("x", "y"))
+    surface_level = defVar(ds, "surface_level", Float64, ("x", "y"))
+    max_oxidation_depth = defVar(ds, "max_oxidation_depth", Float64, ("x", "y"))
+    max_shrinkage_depth = defVar(ds, "max_shrinkage_depth", Float64, ("x", "y"))
+
+    geology .= 1
+    lithology .= 2
+    phreatic .= 0.5
+    thickness .= 0.25
+    base .= 0.0
+    domainbase .= 0.0
+    surface_level .= 1.0
+    max_oxidation_depth .= 1.2
+    max_shrinkage_depth .= 1.2
+    return filename
+end
+
+
+function params_table()
+    filename = tempname()
+    df = DataFrame(
+        geology_name=["NAWA"],
+        lithology_name=["sand"],
+        geology=[1],
+        lithology=[2],
+        gamma_wet=[15000.0],
+        gamma_dry=[10000.0],
+        drainage_factor=[2.0],
+        c_v=[0.006912],
+        a=[0.01737],
+        b=[0.1303],
+        c=[0.008686],
+        ocr=[2.15],
+        mass_fraction_organic=[0.2],
+        minimal_mass_fraction_organic=[0.05],
+        oxidation_rate=[0.001],
+        rho_bulk=[1000.0],
+        mass_fraction_lutum=[0.7],
+        shrinkage_degree=[1.4]
+    )
+    CSV.write(filename, df)
+    return filename
+end
+
+
+path_nc = subsoil_netcdf()
+path_csv = params_table()
 
 
 model = Atlans.Model(
@@ -18,14 +100,3 @@ model = Atlans.Model(
     path_csv,
 )
 
-additional_times = map(DateTime, ["2020-01-01", "2025-01-01", "2030-01-01", "2035-01-01"])
-
-simulation = Atlans.Simulation(
-    model,
-    "examples/output.nc",
-    DateTime("2040-01-01"),
-    forcing,
-    additional_times,
-)
-
-Atlans.run!(simulation)
