612173e730a1047e127e2bb3ddcba5f3beb0b577
smknaake <bas.knaake@deltares.nl>
2025-02-26 11:45:56 +0100

    Removed strange m_minimum_organic reference

--- test/test_carbon_store.jl
+++ test/test_carbon_store.jl
@@ -1,30 +1,29 @@
 @testset "CarbonStore" begin
-    f_organic = 0.2
-    ρb = 1000.0
-    Δz = 1.0
-    f_minimum_organic = 0.05
-    α = 1.0e-3
+	f_organic = 0.2
+	ρb = 1000.0
+	Δz = 1.0
+	f_minimum_organic = 0.05
+	α = 1.0e-3
 
-    m_minimum_organic =
-        cell = Atlans.CarbonStore(
-            Δz,
-            f_organic,
-            f_minimum_organic,
-            Atlans.mass_organic(f_organic, ρb, Δz),
-            Atlans.mass_mineral(f_organic, ρb, Δz),
-            α,
-            α,
-            0.0,
-        )
+	cell = Atlans.CarbonStore(
+		Δz,
+		f_organic,
+		f_minimum_organic,
+		Atlans.mass_organic(f_organic, ρb, Δz),
+		Atlans.mass_mineral(f_organic, ρb, Δz),
+		α,
+		α,
+		0.0,
+	)
 
-    @testset "Initialization" begin
-        @test typeof(cell) == Atlans.CarbonStore
-    end
+	@testset "Initialization" begin
+		@test typeof(cell) == Atlans.CarbonStore
+	end
 
-    @testset "oxidate" begin
-        Δt = 1.0
-        new_cell = Atlans.oxidate(cell, Δt)
-        @test typeof(new_cell) == Atlans.CarbonStore
-        @test new_cell.oxidation ≈ 2.5e-6
-    end
+	@testset "oxidate" begin
+		Δt = 1.0
+		new_cell = Atlans.oxidate(cell, Δt)
+		@test typeof(new_cell) == Atlans.CarbonStore
+		@test new_cell.oxidation ≈ 2.5e-6
+	end
 end
